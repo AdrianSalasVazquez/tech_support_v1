8f6638c127bb62a413fa205bdc9d4f48e97f436f
Adrian <adriansalasvazquez@gmail.com>
2021-01-21 12:16:27 +0100

    Añadido el poner espacios en el bye

--- SupportSystem.java
+++ SupportSystem.java
@@ -38,8 +38,7 @@ public class SupportSystem
 
         while(!finished) {
             String input = reader.getInput();
-
-            if(input.startsWith("bye")) {
+            if(input.trim().startsWith("bye")) {
                 finished = true;
             }
             else {
