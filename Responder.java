9809d20d172441d60f3e038aa7ac4d25cc3a7d62
Adrian <adriansalasvazquez@gmail.com>
2021-01-25 09:23:12 +0100

    añadido respuestas aleatorias

--- Responder.java
+++ Responder.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+import java.util.ArrayList;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +10,22 @@
  */
 public class Responder
 {
+    
+    private Random random;
+    private ArrayList<String> respuestas;
+    
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        random = new Random();
+        respuestas = new ArrayList<String>();
+        respuestas.add("That sounds interesting. Tell me more...");
+        respuestas.add("Ok, I understand...");
+        respuestas.add("I will look at it...");
+        respuestas.add("Keep telling me everything...");
+        respuestas.add("Tell me everything that I will do everything I can...");
     }
 
     /**
@@ -20,6 +34,7 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        String respuesta = respuestas.get(random.nextInt(respuestas.size()));
+        return respuesta;
     }
 }
